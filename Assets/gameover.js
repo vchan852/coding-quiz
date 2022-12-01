ba970cb491edde62222eb2572e0cabde7a84ffe4
Vanessa Chan <vchan@Vanessas-MacBook-Air.local>
2022-12-01 01:45:07 -0800

    relabled class and ids

--- Assets/gameover.js
+++ Assets/gameover.js
@@ -1,1 +1,6 @@
 // <!-- GAMEOVER JS TO DISPLAY AND SAVE SCORE AND SAVE INITIAL OF PLAYER -->
+
+
+
+// we need EventListeners to rerun quiz back to original html
+
--- Assets/script.js
+++ Assets/script.js
@@ -1,29 +1,31 @@
 // QUESTIONS AND ANSWERS FOR QUIZ
 var question = [
 
- { question: "What symbols are used to display a function?",
- a : "[] and {}",
- b : "() and []",
- c : "{} and []",
- answer: "c",
- }
+    { question: "What symbols are used to display a function?",
+    a : "[] and {}",
+    b : "() and []",
+    c : "{} and []",
+    answer: "c",
+    }
+   
+    { question: "What symbol is used to demonstrate a class in css?",
+    a : "#",
+    b : ".",
+    c : "*",
+    answer: "b",
+    }
+   
+    { question: 3
+   
+    }
+   
+    { question: 4
+   
+    }
+   
+    { question: 5
+   
+    }
+   ]
 
- { question: "What symbol is used to demonstrate a class in css?",
- a : "#",
- b : ".",
- c : "*",
- answer: "b",
- }
 
- { question: 3
-
- }
-
- { question: 4
-
- }
-
- { question: 5
-
- }
-]
