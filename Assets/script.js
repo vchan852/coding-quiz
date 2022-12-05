28f12e07d957cad057ce4f37abd4e43b8b7fe292
Vanessa Chan <vchan@Vanessas-MacBook-Air.local>
2022-12-05 15:01:02 -0800

    added coding quiz questions

--- Assets/script.js
+++ Assets/script.js
@@ -18,23 +18,33 @@ var question = [
     c : "{} and []",
     answer: "c",
     },
-    { question: "What symbol is used to demonstrate a class in css?",
-    a : "#",
-    b : ".",
-    c : "*",
+    { question: "What is the correct syntax for referring to an external script called "xxx.js"?",
+    a : "<script src="xxx.js">",
+    b : "<script name="xxx.js">",
+    c : "<script href="xxx.js">",
+    answer: "a",
+    },
+   
+    { question: "How do you write "Stream Indigo" in an alert box?",
+    a : "alert("Stream Indigo")",
+    b : "alertBox="Stream Indigo",
+    c : "alertBox("Stream Indigo")",
+    answer: "a",
+    },
+   
+    { question: "What is the correct way to write a JavaScript array?",
+    a : "avar txt = new Array:1=("joshua")2=("dk")3=("tae")",
+    b : "var txt = new Array("joshua","dk","tae")",
+    c : "var txt = new Array="joshua","dk","tae"",
     answer: "b",
     },
    
-    // { question: 3
-   
-    // }
-   
-    // { question: 4
-   
-    // }
-   
-    // { question: 5
-   
-    // }
+    { question: "How can you add a comment in a JavaScript?",
+    a : "#This is a comment",
+    b : "<!--This is a comment-->",
+    c : "//This is a comment",
+    answer: "c",
+    },
+
    ];
 
