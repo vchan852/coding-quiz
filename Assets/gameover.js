a9c78349e00b0e8fdebaf822f0e467ce12395417
Vanessa Chan <vchan@Vanessas-MacBook-Air.local>
2022-12-05 23:06:19 -0800

    javascripts and touches of css

--- Assets/gameover.js
+++ Assets/gameover.js
@@ -1,6 +1,80 @@
 // <!-- GAMEOVER JS TO DISPLAY AND SAVE SCORE AND SAVE INITIAL OF PLAYER -->
+let currentLeader = localStorage.getItem("currentLeader");
+let currentScore = localStorage.getItem("currentScore");
+let currentHighScore = localStorage.getItem("currentHighScore");
+
+// storing information in application inspect
+
+let currentLeaderEl = document.querySelectorAll(".current-leader");
+let currentScoreEl = document.querySelectorAll(".current-score");
+
+let currentLeaderScoreEl = document.querySelector("#game-high-score");
+let alertWinLoseEl = document.querySelector("#win-lose");
+
+
+// needs a form to submit winner name and play again button
+let formEl = document.querySelector("form");
+
+let inputPlayerBoxEl = document.querySelector("#player-input-box");
+let inputNameEl = document.querySelector("#player-name");
+let submitNameButton = document.querySelector("#submit-name-button");
+
+
+let playAgainButton = document.querySelector("#play-again-button");
+
+let endGameHighScore = document.querySelector("#game-high-score");
 
 
 
 // we need EventListeners to rerun quiz back to original html
 
+currentLeaderEl.forEach((noa) => {
+    noa.textContent = currentLeader;
+});
+
+currentScoreEl.forEach((noa) => {
+    noa.textContent = currentScore;
+});
+
+if (!currentLeader) {
+    alertWinLoseEl.textContent =
+        "You're the top player";
+    currentLeaderEl.forEach((noa) => {
+        noa.textContent = "Lets Begin";
+    });
+
+    currentLeaderScoreEl.textContent = "Lets Begin";
+    localStorage.setItem("currentHighScore", currentScore);
+
+} else {
+    endGameHighScore.textContent = currentHighScore;
+    currentLeaderEl.textContent = currentLeader + " - " + currentHighScore;
+
+    // DETERMINE IF PLAYER IS WINNER 
+
+    if (currentScore > currentHighScore) {
+        currentLeaderEl.textContent = "Enter Your Initials";
+        alertWinLoseEl.textContent =
+            "You're in the lead";
+        localStorage.setItem("currentHighScore", currentScore);
+    } else {
+        alertWinLoseEl.textContent = "Lets play again";
+        playAgainButton.style.display = "inherit";
+        inputPlayerBoxEl.style.display = "none";
+    }
+}
+
+removeCurrentScore = () => {
+    localStorage.removeItem("currentScore");
+};
+
+submitNameButton.addEventListener("click", () => {
+    removeCurrentScore();
+    localStorage.setItem("currentLeader", inputNameEl.value);
+    document.location.assign("index.html");
+});
+
+playAgainButton.addEventListener("click", () => {
+    removeCurrentScore();
+    document.location.assign("index.html");
+});
--- Assets/script.js
+++ Assets/script.js
@@ -9,6 +9,32 @@ const rightAnswer = 10;
 const wrongAnswer = 10;
 
 
+let timeLeftEl = document.querySelector("#countdown");
+let timeLeftBoxEl = document.querySelector("#time-left-box");
+
+let currentLeaderEl = document.querySelectorAll(".current-leader");
+let currentLeaderScoreEl = document.querySelector("#current-leader");
+let currentScoreEl = document.querySelector("#current-score");
+
+let gameEl = document.querySelector("#start-game");
+let gameHighScoreEl=document.querySelector("#game-high-score");
+
+let startGameButton = document.querySelector("#start-game-button");
+
+let questionEl = document.querySelector("#question");
+let abButtons = document.querySelectorAll(".ab-button");
+let buttons = document.querySelectorAll(".button");
+
+
+let rightWrongEl = document.querySelector("#right-wrong");
+
+// LOCAL STORAGE SECTION
+let currentLeader = localStorage.getItem("currentLeader");
+let currentScore = localStorage.getItem("currentScore");
+let currentHighScore = localStorage.getItem("currentHighScore");
+
+
+
 // QUESTIONS AND ANSWERS FOR QUIZ
 var question = [
 
@@ -18,24 +44,24 @@ var question = [
     c : "{} and []",
     answer: "c",
     },
-    { question: "What is the correct syntax for referring to an external script called "xxx.js"?",
-    a : "<script src="xxx.js">",
-    b : "<script name="xxx.js">",
-    c : "<script href="xxx.js">",
+    { question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    a : "<script src='xxx.js'>",
+    b : "<script name='xxx.js'>",
+    c : "<script href='xxx.js'>",
     answer: "a",
     },
    
-    { question: "How do you write "Stream Indigo" in an alert box?",
-    a : "alert("Stream Indigo")",
-    b : "alertBox="Stream Indigo",
-    c : "alertBox("Stream Indigo")",
+    { question: "How do you write 'Stream Indigo' in an alert box?",
+    a : "alert('Stream Indigo')",
+    b : "alertBox='Stream Indigo",
+    c : "alertBox('Stream Indigo')",
     answer: "a",
     },
    
     { question: "What is the correct way to write a JavaScript array?",
-    a : "avar txt = new Array:1=("joshua")2=("dk")3=("tae")",
-    b : "var txt = new Array("joshua","dk","tae")",
-    c : "var txt = new Array="joshua","dk","tae"",
+    a : "avar txt = new Array:1=('joshua')2=('dk')3=('tae')",
+    b : "var txt = new Array('joshua','dk','tae')",
+    c : "var txt = new Array='joshua','dk','tae'",
     answer: "b",
     },
    
@@ -48,3 +74,124 @@ var question = [
 
    ];
 
+// UPDATE LEADER IN NAV BAR
+
+// updates variables with current information
+currentLeaderEl.forEach((el) => {
+    el.textContent = currentLeader;
+});
+
+setLeader = () => {
+    localStorage.setItem("currentLeader", currentScore);
+};
+
+if (!currentLeader) {
+    currentLeaderEl.forEach((el) => {
+        el.textContent = "?";
+    });
+} else {
+    currentScoreEl.textContent = currentLeader + " -> " + currentHighScore;
+    currentLeaderScoreEl.textContent = currentHighScore;
+}
+
+currentScoreEl.textContent = "?";
+
+
+// GAME START
+startGame = () => { 
+    currentScoreEl.textContent = "Lets Begin";
+    outOfTime = false;
+    score = 0;
+    questionsPool = [...question];
+
+    populateNextQuestion();
+ 
+    gameEl.style.display = "none";
+};
+
+// END OF GAME
+checkGameOver = () => {
+    if (outOfTime) {
+        gameOver = true;
+
+        buttons.forEach((button) => {
+            button.style.display = "none";
+        });
+        return setInterval(() => {
+            location.assign("./gameover.html");
+        }, 1000);
+    }
+    if (questionsPool.length === 0) {
+        gameOver = true;
+        return location.assign("./gameover.html");
+    }
+};
+
+populateNextQuestion = () => {
+    checkGameOver();
+
+    // GAME QUESTIONS 
+    let questionIndex = Math.floor(Math.random() * questionsPool.length);
+    currentQuestion = questionsPool[questionIndex];
+    questionEl.textContent = currentQuestion.question;
+    abButtons.forEach((abButton) => {
+        let property = abButton.dataset["property"];
+        abButton.innerText = currentQuestion[property];
+        if (abButton.innerText === "undefined") {
+            abButton.parentElement.style.display = "none";
+        } else {
+            abButton.parentElement.style.display = "inherit";
+        }
+    });
+    questionsPool.splice(questionIndex, 1);
+};
+
+// NEED TO ADD THE ANSWERS , USE EVENTLISTENER
+abButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        let selectedAnswer = e.target;
+
+        let selectedAnswerProperty = selectedAnswer.dataset["property"];
+        rightWrongEl.style.display = "inherit";
+
+
+        if (selectedAnswerProperty == currentQuestion.answer) {
+            //rightWrongEl.textContent = "Right";
+            score += rightAnswer;
+            
+            localStorage.setItem("currentScore", score);
+
+            // update the container
+            currentScoreEl.textContent = score;
+        } else {
+            //rightWrongEl.textContent = "Wrong";
+
+            // we need to deduct time
+            timeLeft -= wrongAnswer;
+        }
+        setTimeout(() => {
+            rightWrongEl.style.display = "none";
+        }, 1000);
+
+        populateNextQuestion();
+    });
+});
+
+startCountdown = () => {
+    timeLeftEl.textContent = "Remaining Time : " + timeLeft;
+    let timerCountdown = setInterval(() => {
+        timeLeftEl.textContent = "Remaining Time : " + (timeLeft - 1);
+        timeLeft--;
+        if (timeLeft < 0) {
+            timeLeftEl.textContent = "Time is Up";
+            clearInterval(timerCountdown);
+            outOfTime = true;
+            return checkGameOver();
+        }
+    }, 1000);
+};
+
+startGameButton.addEventListener("click", () => {
+    startCountdown();
+    startGame();
+});
